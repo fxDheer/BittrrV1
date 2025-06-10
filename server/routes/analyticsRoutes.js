8ead26fc89af27350f43effc73c1c13ab52655ce
Withus <livedreamstudio3d@gmail.com>
2025-06-10 17:50:04 +0530

    Fix: export empty routers for reportRoutes and analyticsRoutes

--- server/routes/analyticsRoutes.js
+++ server/routes/analyticsRoutes.js
@@ -1,1 +1,6 @@
- 
+const express = require('express');
+const router = express.Router();
+
+// Add your analytics routes here in the future
+
+module.exports = router; 
--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -1,1 +1,6 @@
- 
+const express = require('express');
+const router = express.Router();
+
+// Add your report routes here in the future
+
+module.exports = router; 
