f679c7b3881f55f9ecea79712fdb7f9930757bea
Withus <livedreamstudio3d@gmail.com>
2025-06-21 14:40:41 +0530

    fix: Correctly import auth middleware in call routes

--- server/routes/callRoutes.js
+++ server/routes/callRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 const { validate } = require('../middleware/validator');
 const { apiLimiter } = require('../middleware/rateLimiter');
 const Call = require('../models/Call');
